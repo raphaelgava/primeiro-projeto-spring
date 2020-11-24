507e9ad4a1aec66abeb688f56b84ace2f71bff7f
raphaelgava <raphaelgava1@gmail.com>
2020-11-24 15:04:38 -0300

    Segundo commit

--- src/main/java/com/example/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/example/api/MeuPrimeiroProjetoApplication.java
@@ -11,6 +11,7 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
 	}
 
 }
