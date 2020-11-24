c60c6964f3197586a28ec078e21280e266de613c
raphaelgava <raphaelgava1@gmail.com>
2020-11-24 14:50:07 -0300

    primeiro commit

---
+++ src/main/java/com/example/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,16 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Anotação para preparar/inicializar o todo o spring
+@SpringBootApplication
+
+//Para rodar: Run As -> Spring Boot App
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
